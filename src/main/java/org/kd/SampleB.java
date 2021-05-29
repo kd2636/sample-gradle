0f94e3c657fed7ffd5e4cd9dc6182ca133fd0db4
Kushagra <dalmia.kushagra@gmail.com>
2021-05-29 21:03:05 +0530

    reactive hello done

--- src/main/java/org/kd/SampleB.java
+++ src/main/java/org/kd/SampleB.java
@@ -1,7 +1,11 @@
 package org.kd;
 
+import io.reactivex.rxjava3.core.Flowable;
+
 public class SampleB {
     public static void main(String[] args) {
         System.out.println("SampleB class created");
+        Flowable.just("Hello, world!!")
+                .subscribe(System.out::println);
     }
 }
